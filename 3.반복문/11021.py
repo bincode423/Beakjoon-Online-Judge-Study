56491e6bad539bbdfc3ca14801bcbb53e6b62513
bindobi <155980122+bindobi@users.noreply.github.com>
2024-02-27 05:04:17 +0000

    더 짧게 줄이긴 했는데 시간이...

--- 3.반복문/11021.py
+++ 3.반복문/11021.py
@@ -4,3 +4,9 @@ for i in range(n):
     nums.append(list(map(int,input().split())))
 for i in range(n):
     print(f"Case #{i+1}: {nums[i][0]+nums[i][1]}")
+
+#효율성 코드?
+n = int(input())
+for i in range(n):
+    x = list(map(int,input().split()))
+    print(f"Case #{i+1}: {x[0]+x[1]}")
